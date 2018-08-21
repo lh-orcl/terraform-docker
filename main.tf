9805b8e38a7e8c9e47fe846089342687d5587ed0
lh-orcl <40397212+lh-orcl@users.noreply.github.com>
2018-08-21 15:20:03 +0100

    nginx image

--- main.tf
+++ main.tf
@@ -3,10 +3,18 @@ provider "docker" {
 }
 
 resource "docker_container" "HelloWorld" {
-  image = "${docker_image.ubuntu.latest}"
+  image = "${docker_image.nginx.latest}"
   name = "Hello"
+  ports {
+    internal = 80
+  }
+  volumes {
+    container_path = "/usr/share/nginx/html"
+    host_path = "/var/www/nginx/terraform-docker/www"
+    read_only = true
+  }
 }
 
-resource "docker_image" "ubuntu" {
-    name = "ubuntu:latest"
+resource "docker_image" "nginx" {
+    name = "nginx:latest"
 }
