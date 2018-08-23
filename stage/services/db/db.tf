e10b34ebcfaca9994153254413c6688204f64878
lh-orcl <40397212+lh-orcl@users.noreply.github.com>
2018-08-23 14:22:09 +0100

    test

--- stage/services/db/db.tf
+++ stage/services/db/db.tf
@@ -14,7 +14,7 @@ resource "docker_container" "HelloDB" {
     host_path = "${var.host_path}"
     read_only = true
   }
-  must_run = "true"
+#  must_run = "true"
 }
 
 resource "docker_image" "mysql" {
