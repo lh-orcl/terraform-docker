4da78075bb9239c73abb4c0a096f33870830f1c4
lh-orcl <40397212+lh-orcl@users.noreply.github.com>
2018-08-23 09:26:07 +0100

    Add vars file

---
+++ stage/services/web/web_vars.tf
@@ -0,0 +1,19 @@
+variable "container_path" {
+  description = "Path to web directory on Docker Host"
+  default = "/var/www/nginx/terraform-docker/www"
+}
+
+variable "host_path" {
+  description = "Web directory in web container"
+  default = "../../../www"
+}
+
+variable "web_count" {
+  description = "Number of webs"
+  default = 2
+}
+
+variable "web_port" {
+  description = "Exposed web port"
+  default = 80
+}
